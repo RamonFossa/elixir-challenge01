a6fa706c61f31cb66847def61cd5292dcb9f5aad
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 13:31:54 -0300

    feat: Implemented module that returns length of a list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    increase_length(list, 0)
+  end
+
+  defp increase_length([_head | tail], acc) do
+    increase_length(tail, acc + 1)
+  end
+
+  defp increase_length([], acc) do
+    acc
   end
 end
